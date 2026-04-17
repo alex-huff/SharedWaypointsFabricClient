aa008c624a715b61e6ae4705cb308d406d42384a
alex-huff <alexhuff@bakahuff.com>
2026-04-17 17:18:59 -0500

    refactor: use vanilla methods for world->gui space conversion

--- src/main/java/dev/phonis/sharedwaypoints/client/mixin/MixinGameRenderer.java
+++ src/main/java/dev/phonis/sharedwaypoints/client/mixin/MixinGameRenderer.java
@@ -1,16 +1,11 @@
 package dev.phonis.sharedwaypoints.client.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.systems.RenderSystem;
 import dev.phonis.sharedwaypoints.client.config.SWConfig;
 import dev.phonis.sharedwaypoints.client.render.WaypointRenderer;
-import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.world.level.dimension.DimensionType;
-import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -27,17 +22,11 @@ public class MixinGameRenderer
             target = "Lnet/minecraft/client/renderer/LevelRenderer;renderLevel(Lcom/mojang/blaze3d/resource/GraphicsResourceAllocator;Lnet/minecraft/client/DeltaTracker;ZLnet/minecraft/client/renderer/state/level/CameraRenderState;Lorg/joml/Matrix4fc;Lcom/mojang/blaze3d/buffers/GpuBufferSlice;Lorg/joml/Vector4f;ZLnet/minecraft/client/renderer/chunk/ChunkSectionsToRender;)V", shift = At.Shift.BEFORE
         )
     )
-    public void onWorldRender(DeltaTracker renderTickCounter, CallbackInfo ci, @Local(ordinal = 0) Matrix4f matrix4f)
+    public void onWorldRender(DeltaTracker renderTickCounter, CallbackInfo ci)
     {
-        CameraRenderState cameraRendererState = Minecraft.getInstance().gameRenderer.getGameRenderState().levelRenderState.cameraRenderState;
-        Matrix4f projectionMatrix = matrix4f;
-        Matrix4f positionMatrix = cameraRendererState.viewRotationMatrix;
-        Matrix4f modelViewMatrix = RenderSystem.getModelViewMatrix();
-
         // Clear any hudRenderTasks from last tick.
         WaypointRenderer.hudRenderTasks.clear();
         Minecraft minecraftClient = Minecraft.getInstance();
-        Camera camera = minecraftClient.gameRenderer.getMainCamera();
 
         if (minecraftClient.level == null)
         {
@@ -48,7 +37,7 @@ public class MixinGameRenderer
 
         if (SWConfig.INSTANCE.renderWaypoints)
         {
-            WaypointRenderer.renderWaypoints(currentDimension, projectionMatrix, positionMatrix, modelViewMatrix, camera);
+            WaypointRenderer.renderWaypoints(currentDimension);
         }
     }
 
